9de085d0f0f959e5e0344c6b4dc5128215744ca0
sharhar <wiishahar>
2021-01-04 15:33:03 -0800

    Added start button

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const http = require('http');
 const fs = require('fs');
+const { exec } = require("child_process");
+const { spawn } = require("child_process");
+
+var start_requested = false;
+var server_running = false;
 
 var raw_html_data = "";
 
@@ -9,49 +14,86 @@ try {
   console.error(err);
 }
 
-var stati = ['<div style="color: red;">Offline</div>', '<div style="color: orange;">Initializing...</div>', '<div style="color: green;">Online</div>'];
-
 var html_sections = raw_html_data.split("___");
 
+var stati = ['<div style="color: red;">Offline</div>', '<div style="color: orange;">Initializing...</div>', '<div style="color: green;">Online</div>'];
+
 const server = http.createServer(function(req, res) {
-	var raw_status_data = "";
+	if(req.method === 'POST') {
+		start_requested = true;
+	} else {
+		var raw_status_data = "";
 
-	try {
-	  raw_status_data = fs.readFileSync('status.json', 'utf8');
-	} catch (err) {
-	  console.error(err);
-	}
-
-	var server_status = JSON.parse(raw_status_data);
-
-	var final_html = html_sections[0];
-
-	final_html += "<h1>FTB Server Status";
-
-	if(server_status.status == 5) {
-		final_html += stati[2] + "</h1><br><h5>Players Currently Online (" + server_status.players.length +")</h5>";
-
-		var player_list = "";
-
-		for(var i = 0; i < server_status.players.length; i++) {
-			player_list += "<br>" + server_status.players[i];
+		try {
+		  raw_status_data = fs.readFileSync('status.json', 'utf8');
+		} catch (err) {
+		  console.error(err);
 		}
 
-		final_html += player_list.substring(4);
+		if(req.url == '/check') {
+			res.end(raw_status_data);
+		} else {
+			var server_status = JSON.parse(raw_status_data);
 
-		console.log(server_status.players);
-	} else if (server_status.status == 0) {
-		final_html += stati[0] + "</h1>"; //TODO: add button to start server
-	} else {
-		final_html += stati[1] + "</h1><br><h5>Server initialization will take several minutes. Please be patient.</h5>";
+			var final_html = html_sections[0];
+
+			final_html += "<h1>FTB Server Status";
+
+			if(server_status.status == 5) {
+				final_html += stati[2] + "</h1><br><h5>Players Currently Online (" + server_status.players.length +")</h5>";
+
+				var player_list = "";
+
+				for(var i = 0; i < server_status.players.length; i++) {
+					player_list += "<br>" + server_status.players[i];
+				}
+
+				final_html += player_list.substring(4);
+			} else if (server_status.status == 0) {
+				final_html += stati[0] + "</h1><button type=\"button\" class=\"btn btn-outline-success btn-lg\" id=\"serverStartButton\" onclick=\"fetch('/click', {method: 'POST'})\">Start Server</button>";
+			} else {
+				final_html += stati[1] + "</h1><br><h5>Server initialization will take several minutes. Please be patient.</h5>";
+			}
+
+			final_html += html_sections[1];
+
+			res.write(final_html);
+			res.end();
+		}
 	}
-
-	final_html += html_sections[1];
-
-	res.write(final_html);
-	res.end();
 });
 
 server.listen(80, function() {
 	console.log('Server running at http://127.0.0.1:80');
 });
+
+function serverStarter() {
+	if(start_requested && !server_running) {
+		server_running = true;
+
+		const proc = spawn("bash", ["start.sh"]);
+
+		proc.stdout.on("data", data => {
+		    console.log(`stdout: ${data}`);
+		});
+
+		proc.stderr.on("data", data => {
+		    console.log(`stderr: ${data}`);
+		});
+
+		proc.on('error', (error) => {
+		    console.log(`error: ${error.message}`);
+		});
+
+		proc.on("close", code => {
+			server_running = false;
+		    console.log(`child process exited with code ${code}`);
+		});
+	}
+
+	start_requested = false;
+
+	setTimeout(serverStarter, 1000); 
+}
+
+setTimeout(serverStarter, 0); 

8fe98f04c9a22dc78fc57b97edefe9df824fce92
sharhar <wiishahar@gmail.com>
2021-01-17 14:04:11 -0800

    Website updating happens on client side instead of server side

--- server.js
+++ server.js
@@ -14,10 +14,6 @@ try {
   console.error(err);
 }
 
-var html_sections = raw_html_data.split("___");
-
-var stati = ['<div style="color: red;">Offline</div>', '<div style="color: orange;">Initializing...</div>', '<div style="color: green;">Online</div>'];
-
 const server = http.createServer(function(req, res) {
 	if(req.method === 'POST') {
 		if(req.url == "/click") {
@@ -35,31 +31,7 @@ const server = http.createServer(function(req, res) {
 		if(req.url == '/check') {
 			res.end(raw_status_data);
 		} else {
-			var server_status = JSON.parse(raw_status_data);
-
-			var final_html = html_sections[0];
-
-			final_html += "<h1>FTB Server Status";
-
-			if(server_status.status == 5) {
-				final_html += stati[2] + "</h1><br><h5>Players Currently Online (" + server_status.players.length +")</h5>";
-
-				var player_list = "";
-
-				for(var i = 0; i < server_status.players.length; i++) {
-					player_list += "<br>" + server_status.players[i];
-				}
-
-				final_html += player_list.substring(4);
-			} else if (server_status.status == 0) {
-				final_html += stati[0] + "</h1><button type=\"button\" class=\"btn btn-outline-success btn-lg\" id=\"serverStartButton\" onclick=\"fetch('/click', {method: 'POST'})\">Start Server</button>";
-			} else {
-				final_html += stati[1] + "</h1><br><h5>Server initialization will take several minutes. Please be patient.</h5>";
-			}
-
-			final_html += html_sections[1];
-
-			res.write(final_html);
+			res.write(raw_html_data);
 			res.end();
 		}
 	}
